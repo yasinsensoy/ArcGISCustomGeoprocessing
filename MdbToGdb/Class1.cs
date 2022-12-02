ec548612414290d52be9c84909dd44e77c26666e
Yasin Şensoy <ysensoy@konumsal.com.tr>
2022-12-02 10:03:21 +0300

    Add project files.

---
+++ MdbToGdb/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MdbToGdb
+{
+    public class Class1
+    {
+    }
+}
